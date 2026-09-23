e0a622b411ff651a9944a3e2598c023af0939dd8
Tomas Guerra <tomiguerra981@gmail.com>
2026-09-23 19:24:43 -0300

    TP5 Empezado y Terminado (23/09)

---
+++ Assets/Scripts TP4/NPCMovement.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+
+public class NPCMovement : MonoBehaviour
+{
+    [SerializeField] Transform Player;
+    [SerializeField] float distanciaPersecucion = 10f;
+    [SerializeField] float velocidad = 3f;
+
+    Rigidbody rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        float distancia = Vector3.Distance(transform.position, Player.position);
+
+        if (distancia <= distanciaPersecucion)
+        {
+            Vector3 direccion = Player.position - transform.position;
+            direccion.y = 0;
+
+            rb.linearVelocity = direccion.normalized * velocidad;
+        }
+        else
+        {
+            rb.linearVelocity = Vector3.zero;
+        }
+    }
+
+
+    void OnDrawGizmos()
+    {
+        Gizmos.DrawWireSphere(transform.position, distanciaPersecucion);
+    }
+}
